f21a5637868fd75363988bfbb95ccaed05563d2d
chetannn-github <chetan.rajawat25@gmail.com>
2025-07-02 18:46:27 +0530

    setup server using express,mobile app using expo and website using react.js

---
+++ server/server.js
@@ -0,0 +1,22 @@
+import express from "express"
+import "dotenv/config"
+import authRouter from "./routes/auth.routes.js";
+import { connectToDb } from "./config/db.config.js";
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.get("/api/test", (req,res)=>{
+    return res.json({"message" : "server is running fine"});
+})
+
+app.use("/api/auth", authRouter)
+
+
+app.listen(PORT, ()=> {
+    connectToDb();
+    console.log("Server is running on port " + PORT)
+})
