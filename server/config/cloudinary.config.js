99c8fed76587480fa7c83fffd70df80d3d220c58
chetannn-github <chetan.rajawat25@gmail.com>
2025-07-02 19:47:50 +0530

    refactor: remove unused deleteImage function from Cloudinary configuration

--- server/config/cloudinary.config.js
+++ server/config/cloudinary.config.js
@@ -7,16 +7,6 @@ cloudinary.config({
     api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
-export async function deleteImage (imageURL) {
-    try {
-        let cloudinaryImageId = imageURL.split("/").pop().split(".")[0];
-        return await cloudinary.uploader.destroy(cloudinaryImageId);
-    } catch (error) {
-        console.log("error in deleting image from cloudinary");
-    }
-    
-}
-
 
 export async function uploadImage (image) {
   return await cloudinary.uploader.upload(image);
