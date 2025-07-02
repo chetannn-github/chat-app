0afb95d132aa4deca2c747bc4e8c8620465971f2
chetannn-github <chetan.rajawat25@gmail.com>
2025-07-02 21:09:19 +0530

    fix: correct reference for sender and receiver fields in message schema

--- server/models/message.model.js
+++ server/models/message.model.js
@@ -4,10 +4,12 @@ import mongoose from "mongoose"
 const messageSchema = new mongoose.Schema({
     sender : {
         type : mongoose.Schema.Types.ObjectId,
+        ref : "User",
         required:true,
     },
     reciever : {
         type : mongoose.Schema.Types.ObjectId,
+        ref : "User",
         required:true,
     },
     image : {
